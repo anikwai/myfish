a64ea3fdfb09ace55a036e7fdb9887ebe937f47b
anikwai <anikwai.w@gmail.com>
2026-04-02 10:18:13 +1100

    style(layout): add content padding to app sidebar layout

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -13,7 +13,7 @@ export default function AppSidebarLayout({
             <AppSidebar />
             <AppContent variant="sidebar" className="overflow-x-hidden">
                 <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                {children}
+                <div className="px-6 py-6 md:px-8">{children}</div>
             </AppContent>
         </AppShell>
     );
